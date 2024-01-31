f58f6e35ce42949b0e027c609ac58cb66111ba8f
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-02-01 03:44:55 +0630

    [Complete] ToDoList Read, Create, Update, Delete

---
+++ BlazorWasm.ToDoListV2/Models/ToDoListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWasm.ToDoListV2.Models
+{
+    public class ToDoListModel
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+        public bool IsCompleted { get; set; }
+        public string TaskName { get; set; }
+    }
+}
---
+++ BlazorWasm.ToDoListV2/Pages/ToDoListPage.razor.cs
@@ -0,0 +1,33 @@
+﻿using BlazorWasm.ToDoListV2.Models;
+
+namespace BlazorWasm.ToDoListV2.Pages
+{
+    public partial class ToDoListPage
+    {
+        private static List<ToDoListModel> data = new List<ToDoListModel>();
+        private ToDoListModel reqModel { get; set; } = new ToDoListModel();
+
+        private void Add()
+        {
+            data.Add(reqModel);
+            reqModel = new ToDoListModel();
+        }
+
+        private void Cancel()
+        {
+            reqModel = new ToDoListModel();
+        }
+
+        private void ToggleTask(ToDoListModel item)
+        {
+            int index = data.FindIndex(x => x.Id == item.Id);
+            data[index].IsCompleted = !data[index].IsCompleted;
+            Console.WriteLine(data[index].IsCompleted);
+        }
+
+        private void Delete(string id)
+        {
+            data = data.Where(x => x.Id != id).ToList();
+        }
+    }
+}
